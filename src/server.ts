c9ed9814fd5dd1e239979497544f4d53fad3c1ff
josephDev123 <josephuzuegbu55@gmail.com>
2025-02-27 15:30:54 +0100

    creating models

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from './app';
-import { env } from './config/env';
-import logger from './utils/logger';
+import app from "./app";
+import { env } from "./config/env";
+import logger from "./utils/logger";
 
 const PORT = env.PORT;
 app.listen(PORT, () => {
