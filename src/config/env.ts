72d912ba03b171bfea6d78dc04053b9593c2feda
Kelechi Ugwu <kcblack22@gmail.com>
2025-03-20 21:34:58 +0100

    Used axios for bravo instead

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,7 +6,7 @@ export const env = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || "3000",
   JWT_SECRET: process.env.JWT_SECRET,
-  brevoApiKey: process.env.BREVO_API_KEY as string,
+  BREVO_API_KEY: process.env.BREVO_API_KEY as string,
   DATABASE_URL:process.env.DATABASE_URL as string,
   EMAIL:process.env.EMAIL_SENDER,
   FACEBOOK_ID:process.env.FACEBOOK_APP_ID,
