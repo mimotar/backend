eb020a49f4945572b28e42f906b1f19edba09c50
Kelechi Ugwu <kcblack22@gmail.com>
2026-04-10 18:19:07 +0100

    Updating ...

--- src/services/dashboard/dashboard.service.ts
+++ src/services/dashboard/dashboard.service.ts
@@ -116,9 +116,9 @@ export async function DashboardService(id: number, months?: number) {
     userEarnings.forEach(e => {
         const period = e.createdAt.toISOString().slice(0, 7);
         if (amountPerPeriod[period] !== undefined) {
-             amountPerPeriod[period] += e.amount;
+             amountPerPeriod[period] += e.amount.toNumber();
         } else if (!months) {
-             amountPerPeriod[period] = e.amount;
+             amountPerPeriod[period] = e.amount.toNumber();
         }
     });
 
